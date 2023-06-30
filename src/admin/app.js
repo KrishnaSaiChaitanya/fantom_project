c512f8a9ebe8ba149fa0bc134be5f50d2eeab35d
chaitanya <chaitanya.tata215@gmail.com>
2023-06-30 22:19:49 +0530

    Added cloudinary database

---
+++ src/admin/app.js
@@ -0,0 +1,72 @@
+// const config = {
+//   locales: [
+//     // 'ar',
+//     // 'fr',
+//     // 'cs',
+//     // 'de',
+//     // 'dk',
+//     // 'es',
+//     // 'he',
+//     // 'id',
+//     // 'it',
+//     // 'ja',
+//     // 'ko',
+//     // 'ms',
+//     // 'nl',
+//     // 'no',
+//     // 'pl',
+//     // 'pt-BR',
+//     // 'pt',
+//     // 'ru',
+//     // 'sk',
+//     // 'sv',
+//     // 'th',
+//     // 'tr',
+//     // 'uk',
+//     // 'vi',
+//     // 'zh-Hans',
+//     // 'zh',
+//   ],
+// };
+
+// const bootstrap = (app) => {
+//   console.log(app);
+// };
+
+// export default {
+//   config,
+//   bootstrap,
+// };
+
+export default {
+  config: {
+    theme: {
+      dark: {
+        colors: {
+          // primary100: "#c9ffeb",
+          primary100: "#181826",
+          primary200: "#5effc3",
+          primary500: "#00f298",
+          buttonPrimary500: "#ffe298",
+          primary600: "#00bd77",
+          buttonPrimary600: "#ffbd77",
+          primary700: "#008755",
+        },
+      },
+    },
+    locales: ["pt-BR"],
+    tutorials: false,
+    notifications: {
+      releases: false,
+    },
+    translations: {
+      en: {
+        "app.components.LeftMenu.navbrand.title": "Dashboard",
+      },
+      "pt-BR": {
+        "app.components.LeftMenu.navbrand.title": "Painel",
+      },
+    },
+  },
+  bootstrap() {},
+};
